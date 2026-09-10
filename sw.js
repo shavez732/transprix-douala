14f8108ee408ce032698af3098a7d6e7f65c7f9c
shavez732 <shavezlicence@gmail.com>
2026-09-10 23:39:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'transprix-v1';
+const CACHE_NAME = 'transprix-v2';
 const ASSETS = [
+  '/transprix-douala/',
   '/transprix-douala/index.html',
   '/transprix-douala/manifest.json'
 ];
 
-// Enregistrement des fichiers dans la mémoire du téléphone (Cache)
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -13,22 +13,18 @@ self.addEventListener('install', (e) => {
   );
 });
 
-// Activation et nettoyage des vieux caches
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       );
     })
   );
 });
 
-// Récupération de la page depuis la mémoire si l'utilisateur est hors-ligne
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((cachedResponse) => {
